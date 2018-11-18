8d6b759fcef25f0377fa807be420705d6709d8c2
Valvrave <gabrielmarques.mg@gmail.com>
2018-11-18 12:23:09 -0200

    Add -> Classe Fita
    Adicionei a classe Fita.

---
+++ ProdInCena/src/prodincena/Fita.java
@@ -0,0 +1,33 @@
+package prodincena;
+
+public class Fita {
+    String numSerie;
+    double capMinutos;
+
+    // Builder's
+    public Fita(){
+    
+    }
+    
+    public Fita(String numSerie, double capMinutos) {
+        this.numSerie = numSerie;
+        this.capMinutos = capMinutos;
+    }
+    
+    // Get's and Set's
+    public String getNumSerie() {
+        return numSerie;
+    }
+
+    public void setNumSerie(String numSerie) {
+        this.numSerie = numSerie;
+    }
+
+    public double getCapMinutos() {
+        return capMinutos;
+    }
+
+    public void setCapMinutos(double capMinutos) {
+        this.capMinutos = capMinutos;
+    }
+}
