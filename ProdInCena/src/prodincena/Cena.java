253ced226dbfd38ade2e7c5977c4e4596a51e7ef
Valvrave <gabrielmarques.mg@gmail.com>
2018-11-18 12:09:45 -0200

    Add -> Classe Cena.cod
    Codigos da classe cena.

--- ProdInCena/src/prodincena/Cena.java
+++ ProdInCena/src/prodincena/Cena.java
@@ -2,5 +2,32 @@ package prodincena;
 
 public class Cena {
     double duracaoFita;
+    int quantFita;
+
+    // Builder's
+    public Cena(){
     
+    }
+    
+    public Cena(double duracaoFita, int quantFita) {
+        this.duracaoFita = duracaoFita;
+        this.quantFita = quantFita;
+    }
+    
+    // Get's and Set's
+    public double getDuracaoFita() {
+        return duracaoFita;
+    }
+
+    public void setDuracaoFita(double duracaoFita) {
+        this.duracaoFita = duracaoFita;
+    }
+
+    public int getQuantFita() {
+        return quantFita;
+    }
+
+    public void setQuantFita(int quantFita) {
+        this.quantFita = quantFita;
+    }
 }
