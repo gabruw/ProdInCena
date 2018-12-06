49edfae4eb4da6fedf5f7b51d8ead4f2004685c5
Valvrave <gabrielmarques.mg@gmail.com>
2018-12-06 07:26:06 -0200

    Ajuste ShowBanda
    Retirar Comentários desnecessários.

--- ProdInCena/src/prodincena/ShowBanda.java
+++ ProdInCena/src/prodincena/ShowBanda.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prodincena;
 
 import java.text.DateFormat;
 import java.util.ArrayList;
 
-/**
- *
- * @author Leonardo
- */
 public class ShowBanda extends Produção {
     boolean IsLeiIncentivo = false;
     
